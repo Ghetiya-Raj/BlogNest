f657964c20e3c69e6cee933b77b74c3517e12012
Raj Ghetiya <rajghetiya5@gmail.com>
2025-04-02 12:33:47 +0530

    Initial commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const dbConnection = () =>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() =>{
+        console.log("Connected to database!");
+    })
+    .catch((err) =>{
+        console.log(`Error occure while connected to database ${err}`);
+    });
+}
